94272dc98034cd705619e08eefe482fad43dae0a
Uriel Crampton <68357367+urielcrampton@users.noreply.github.com>
2023-08-20 20:00:47 -0300

    Add files via upload

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Desarrollador en varias plataformas web (ASP NET, JavaScript, C# y PHP), Android Nativo y programación de Hardware. Encabezo proyectos de base tecnológica en domótica, desarrollos de tecnologías para educación y desarrollos particulares para empresas.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "PHP",
+  "JavaScript",
+  "Arduino",
+  "C++",
+  "C#",
+  "ASP NET"
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Estudio ingeniería informática en la Universidad de Montevideo (UM). A su vez, tengo una empresa de software y en paralelo otra de hardware e IA. Me gusta trabajar en equipo y aprender nuevas tecnologías. Me apasiona la tecnología y la educación. Me gusta trabajar en proyectos que tengan un impacto positivo en la sociedad";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,17 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Mi página web",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Para ver mi curriculum mire mi página web.",
+    url: "https://www.uriel.website/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Crampton Technology",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+      "Mire mi proyecto tecnológico.",
+    url: "https://crampton.cc/",
+  }
 ];
 
 const Portfolio = () => {
