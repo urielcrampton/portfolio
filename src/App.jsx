4598c8a725a440c59e8c77d1645e73612a3b4b6f
Uriel Crampton <68357367+urielcrampton@users.noreply.github.com>
2023-08-20 19:58:14 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Uriel Crampton",
+  title: "Software & Hardware developer",
+  email: "uriel@ieee.org",
+  gitHub: "urielcrampton",
+  instagram: "urielcrampton",
+  linkedIn: "ucrampton",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#1AB092";
+const secondaryColor = "#16A085";
 
 const App = () => {
   return (
